0125fedc1f9e5115fa38c51c850e9f87637c2459
Harry <poripori1030@gmail.com>
2019-02-17 04:01:14 +0800

    AppRowCell

--- AppStore/Controller/Apps/AppsHorizontalController.swift
+++ AppStore/Controller/Apps/AppsHorizontalController.swift
@@ -15,8 +15,8 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        collectionView.backgroundColor = .blue
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.backgroundColor = .white
+        collectionView.register(AppRowCell.self, forCellWithReuseIdentifier: cellId)
         
         if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
             layout.scrollDirection = .horizontal
@@ -28,8 +28,7 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppRowCell
         return cell
     }
     
@@ -42,7 +41,7 @@ class AppsHorizontalController: BaseListController, UICollectionViewDelegateFlow
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let height = (view.frame.height - (2 * topBottomPadding) - (2 * spacing)) / 3
-        return CGSize(width: view.frame.width, height: height)
+        return CGSize(width: view.frame.width - 48, height: height)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
